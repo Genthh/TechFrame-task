3897fae44a05e626b387e609aadd87306c2a0124
Gent Hulaj <gentihulaj12@gmail.com>
2023-02-28 16:14:42 +0100

    new push

--- src/components/CheckoutPage.js
+++ src/components/CheckoutPage.js
@@ -1,19 +1,48 @@
 import React from "react";
+import "../styles/Checkout.css";
 
 const CheckoutPage = ({ cart }) => {
   const totalPrice = cart.reduce((acc, item) => acc + item.amount * item.price, 0);
+  const VAT = totalPrice * 0.08;
+  let discount = 0;
+  let VATRate = 0.08;
+  let totalWithVAT = totalPrice + VAT;
+
+  if (totalWithVAT > 500) {
+    discount = totalWithVAT * 0.1;
+    totalWithVAT = totalWithVAT - discount;
+    VATRate = 0.16;
+  }
 
   return (
-    <div>
-      <h1>Checkout</h1>
-      {cart.map((item) => (
-        <div key={item.id}>
-          <p>{item.title}</p>
-          <p>{item.amount}</p>
-          <p>{item.price}€</p>
-        </div>
-      ))}
-      <p>Total: {totalPrice}€</p>
+    <div className="invoice">
+      <h1>Invoice</h1>
+      <div className="total">
+      </div>
+      <table>
+        <thead>
+          <tr>
+            <th>Description</th>
+            <th>Amount</th>
+            <th>Discount</th>
+            <th>Price</th>
+            <th>VAT</th>
+            <th>Total</th>
+          </tr>
+        </thead>
+        <tbody>
+          {cart.map((item) => (
+            <tr key={item.id}>
+              <td>{item.title}</td>
+              <td>{item.amount}</td>
+              <td>{discount.toFixed(2)}%</td> 
+              <td>{item.price.toFixed(2)}€</td>
+              <td>({(VATRate * 100).toFixed(0)}%)</td>
+              <td>{(item.amount * item.price + VAT).toFixed(2)}€</td>
+            </tr>
+          ))}
+        </tbody>
+      </table>
     </div>
   );
 };
--- src/components/cart.js
+++ src/components/cart.js
@@ -1,77 +1,32 @@
-import React, { useState, useEffect } from "react";
+import React, { useState } from "react";
 import "../styles/cart.css";
 import CheckoutPage from "./CheckoutPage";
 
-const Cart = ({ cart, setCart, handleChange }) => {
+const Cart = ({ cart, setCart }) => {
   const [price, setPrice] = useState(0);
   const [shouldRedirect, setShouldRedirect] = useState(false);
 
   const handleRemove = (id) => {
     const arr = cart.filter((item) => item.id !== id);
     setCart(arr);
-    handlePrice();
-  };
-  
-  const handlePrice = () => {
-    const governmentRule = {
-      maxInvoiceValue: 500,
-      maxProductQty: 49,
-    };
-    let totalPrice = 0;
-    let invoiceItems = [];
-    let currentQty = 0;
-    let currentPrice = 0;
-    cart.forEach((item) => {
-      if (
-        item.price > 500  ||
-        (currentQty + item.amount > governmentRule.maxProductQty) ||
-        (currentPrice + item.amount * item.price > governmentRule.maxInvoiceValue)
-      ) {
-        while (item.amount > 0) {
-          const maxQty = Math.min(
-            Math.floor((governmentRule.maxInvoiceValue - currentPrice) / item.price),
-            governmentRule.maxProductQty - currentQty,
-            item.amount
-          );
-          const currentItem = { ...item, amount: maxQty };
-          totalPrice += maxQty * currentItem.price;
-          invoiceItems.push(currentItem);
-          item.amount -= maxQty;
-          currentQty += maxQty;
-          currentPrice += maxQty * currentItem.price;
-          if (
-            item.amount === 0 ||
-            currentQty === governmentRule.maxProductQty ||
-            currentPrice === governmentRule.maxInvoiceValue
-          ) {
-            setPrice((prevPrice) => prevPrice + totalPrice);
-            totalPrice = 0;
-            currentQty = 0;
-            currentPrice = 0;
-          }
-        }
-      } else {
-        totalPrice += item.amount * item.price;
-        invoiceItems.push(item);
-        currentQty += item.amount;
-        currentPrice += item.amount * item.price;
-        if (
-          currentQty === governmentRule.maxProductQty ||
-          currentPrice === governmentRule.maxInvoiceValue
-        ) {
-          setPrice((prevPrice) => prevPrice + totalPrice);
-          totalPrice = 0;
-          currentQty = 0;
-          currentPrice = 0;
-        }
-      }
-    });
-    setPrice(totalPrice);
   };
 
-  useEffect(() => {
-    handlePrice();
-  }, [cart]);
+  const handleChange = (item, value) => {
+    const newCart = [...cart];
+    const index = newCart.findIndex((i) => i.id === item.id);
+    if (value === -1 && newCart[index].amount === 1) {
+      handleRemove(item.id);
+    } else if (value === 1 && newCart[index].amount < 50) {
+      newCart[index].amount += 1;
+      setCart(newCart);
+      setPrice(prevPrice => prevPrice + newCart[index].price);
+    } else if (value === 1 && newCart[index].amount >= 50 && newCart[index].price <= 500) {
+      alert(`You cannot add more than 50 items of ${item.title}`);
+    } else if (value === 1 && newCart[index].amount === 1 && newCart[index].price > 500) {
+      alert(`You can only add 1 item of ${item.title} because it costs more than 500 EUR`);
+    }
+  };
+  
 
   const handleCheckout = () => {
     setShouldRedirect(true);
@@ -104,16 +59,200 @@ const Cart = ({ cart, setCart, handleChange }) => {
       <div className="total">
         <span>Total Price of your Cart</span>
         <span>{`${price.toFixed(2)} USD`}</span>
-</div>
-<div className="total">
-<span>VAT (8%)</span>
-<span>{`${VAT.toFixed(2)} USD`}</span>
-</div>
-<button className="checkout_button" onClick={handleCheckout}>
-Checkout
-</button>
-</article>
-);
+      </div>
+      <div className="total">
+        <span>VAT (8%)</span>
+        <span>{`${VAT.toFixed(2)} USD`}</span>
+      </div>
+      <button className="checkout_button" onClick={handleCheckout}>
+        Checkout
+      </button>
+    </article>
+  );
 };
 
 export default Cart;
+
+
+
+// import React, { useState } from "react";
+// import "../styles/cart.css";
+// import CheckoutPage from "./CheckoutPage";
+
+// const Cart = ({ cart, setCart, handleChange }) => {
+//   const [shouldRedirect, setShouldRedirect] = useState(false);
+
+//   const handleRemove = (id) => {
+//     const arr = cart.filter((item) => item.id !== id);
+//     setCart(arr);
+//   };
+//   const handleChanges = (item, value) => {
+//     const newCart = [...cart];
+//     const index = newCart.findIndex((i) => i.id === item.id);
+//     if (value === -1 && newCart[index].amount === 1) {
+//       handleRemove(item.id);
+//     } else if (value === 1 && newCart[index].amount < 50) {
+//       newCart[index].amount += 1;
+//       setCart(newCart);
+//     } else if (value === 1 && newCart[index].amount >= 50 && newCart[index].price <= 500) {
+//       alert(`You cannot add more than 50 items of ${item.title}`);
+//     } else if (value === 1 && newCart[index].amount === 1 && newCart[index].price > 500) {
+//       alert(`You can only add 1 item of ${item.title} because it costs more than 500 EUR`);
+//     }
+//   };  
+
+//   const handleCheckout = () => {
+//     // Check if any item has amount greater than 50
+//     const hasExceededLimit = cart.some((item) => item.amount > 50);
+
+//     if (hasExceededLimit) {
+//       const cartWithinLimit = [];
+//       const cartExceedingLimit = [];
+
+//       // Separate items into two arrays based on amount
+//       cart.forEach((item) => {
+//         if (item.amount <= 50) {
+//           cartWithinLimit.push(item);
+//         } else {
+//           // Create new item with amount <= 50
+//           const newItem = {
+//             ...item,
+//             amount: 50,
+//           };
+//           cartWithinLimit.push(newItem);
+
+//           // Create new item with remaining amount
+//           const remainingAmount = item.amount - 50;
+//           const newItemExceedingLimit = {
+//             ...item,
+//             amount: remainingAmount,
+//           };
+//           cartExceedingLimit.push(newItemExceedingLimit);
+//         }
+//       });
+
+//       // Redirect to checkout page with both cart arrays
+//       setCart(cartWithinLimit);
+//       setShouldRedirect(true);
+
+//       setTimeout(() => {
+//         setCart(cartExceedingLimit);
+//       }, 1000);
+//     } else {
+//       setShouldRedirect(true);
+//     }
+//   };
+
+//   if (shouldRedirect) {
+//     return <CheckoutPage cart={cart} />;
+//   }
+
+//   const totalPrice = cart.reduce(
+//     (acc, item) => acc + item.price * item.amount,
+//     0
+//   );
+//   const VAT = totalPrice <= 500 ? totalPrice * 0.08 : totalPrice * 0.16;
+
+//   return (
+//     <article>
+//       {cart.map((item) => (
+//         <div className="cart_box" key={item.id}>
+//           <div className="cart_img">
+//             <img src={item.img} alt="" />
+//             <p>{item.title}</p>
+//           </div>
+//           <div>
+//             <button onClick={() => handleChange(item, 1)}>+</button>
+//             <button>{item.amount}</button>
+//             <button onClick={() => handleChange(item, -1)}>-</button>
+//           </div>
+//           <div>
+//             <button onClick={() => handleRemove(item.id)}>Remove</button>
+//           </div>
+//         </div>
+//       ))}
+//       <div className="total">
+//         <span>Total Price of your Cart</span>
+//         <span>{`${totalPrice.toFixed(2)} EUR`}</span>
+//       </div>
+//       <div className="total">
+//         <span>VAT ({totalPrice <= 500 ? "8%" : "16%"})</span>
+//         <span>{`${VAT.toFixed(2)} EUR`}</span>
+//       </div>
+//       <button className="checkout_button" onClick={handleCheckout}>
+//         Checkout
+//       </button>
+//     </article>
+//   );
+// };
+
+// export default Cart;
+
+
+
+
+
+// import React, { useState, useEffect } from "react";
+// import "../styles/cart.css";
+// import CheckoutPage from "./CheckoutPage";
+
+// const Cart = ({ cart, setCart, handleChange }) => {
+//   const [price, setPrice] = useState(0);
+//   const [shouldRedirect, setShouldRedirect] = useState(false);
+
+//   const handleRemove = (id) => {
+//     const arr = cart.filter((item) => item.id !== id);
+//     setCart(arr);
+//     // handlePrice();
+//   };
+  
+  
+
+//   // useEffect(() => {
+//   //   handlePrice();
+//   // }, [cart]);
+
+//   const handleCheckout = () => {
+//     setShouldRedirect(true);
+//   };
+
+//   if (shouldRedirect) {
+//     return <CheckoutPage cart={cart} />;
+//   }
+
+//   const VAT = price * 0.08;
+
+//   return (
+//     <article>
+//       {cart.map((item) => (
+//         <div className="cart_box" key={item.id}>
+//           <div className="cart_img">
+//             <img src={item.img} alt="" />
+//             <p>{item.title}</p>
+//           </div>
+//           <div>
+//             <button onClick={() => handleChange(item, 1)}>+</button>
+//             <button>{item.amount}</button>
+//             <button onClick={() => handleChange(item, -1)}>-</button>
+//           </div>
+//           <div>
+//             <button onClick={() => handleRemove(item.id)}>Remove</button>
+//           </div>
+//         </div>
+//       ))}
+//       <div className="total">
+//         <span>Total Price of your Cart</span>
+//         <span>{`${price.toFixed(2)} USD`}</span>
+// </div>
+// <div className="total">
+// <span>VAT (8%)</span>
+// <span>{`${VAT.toFixed(2)} USD`}</span>
+// </div>
+// <button className="checkout_button" onClick={handleCheckout}>
+// Checkout
+// </button>
+// </article>
+// );
+// };
+
+// export default Cart;
